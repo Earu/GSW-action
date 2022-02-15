37f8f762c64db456b74819300f4b560e07c9783c
Earu <earu.derg@gmail.com>
2022-02-15 19:42:22 +0100

    fix debug

---
+++ dist/debug.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+function getDebugInput(input, options) {
+    const debug = JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, "debug.json")).toString());
+    if (debug[input])
+        return debug[input];
+    return "";
+}
+exports.default = getDebugInput;
