af45bbf48ec92a6e0aa53f8012919256420387a8
Earu <earu.derg@gmail.com>
2022-02-15 10:57:57 +0100

    revert to npm fix debugging

--- dist/index.js
+++ dist/index.js
@@ -14,23 +14,27 @@ const publishGMA_1 = __importDefault(require("helpers/publishGMA"));
 const validateFiles_1 = __importDefault(require("helpers/validateFiles"));
 const validateMetaData_1 = __importDefault(require("helpers/validateMetaData"));
 const core_1 = require("@actions/core");
+const DEBUG = false;
+const pgk = DEBUG ? './debug' : '@actions/core';
+const getInput = require(pgk).getInput;
 try {
-    const accountName = (0, core_1.getInput)("account-name");
-    const accountPassword = (0, core_1.getInput)("account-password");
-    const workshopId = (0, core_1.getInput)("workshop-id");
-    const accountSecret = (0, core_1.getInput)("account-secret");
-    const addonPath = (0, core_1.getInput)("addon-path");
+    const accountName = getInput("account-name");
+    const accountPassword = getInput("account-password");
+    const workshopId = getInput("workshop-id");
+    const accountSecret = getInput("account-secret");
+    const addonPath = getInput("addon-path");
     const metadataPath = path_1.default.join(addonPath, "addon.json");
-    if (!fs_1.default.existsSync(metadataPath))
-        (0, core_1.setFailed)("missing addon.json!");
+    if (!fs_1.default.existsSync(metadataPath)) {
+        (0, core_1.setFailed)("Missing addon.json!");
+        process.exit(1);
+    }
     const metadata = JSON.parse(fs_1.default.readFileSync(metadataPath).toString());
     (0, validateMetaData_1.default)(metadata);
     const filePaths = (0, getFilePaths_1.default)(addonPath, metadata.ignore);
     (0, validateFiles_1.default)(filePaths);
     (0, createGMA_1.default)(constants_1.GMA_PATH, metadata.title, (0, buildDescription_1.default)(metadata), filePaths, addonPath);
     let changes = "";
-    // originally this was a "DEBUG" flag, but its preferable we use the NODE_ENV environment vairable instead.
-    if (process.env.NODE_ENV === "development") {
+    if (DEBUG) {
         changes = "DEBUG MESSAGE";
     }
     else if (github_1.context.payload.head_commit && github_1.context.payload.head_commit.message) {
