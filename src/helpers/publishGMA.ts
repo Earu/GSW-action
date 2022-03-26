a54c0fc8fd94c11bb7a2c11b95f81b1a7de43df9
Earu <earu.derg@gmail.com>
2022-03-26 18:56:35 +0100

    fix this whole thing once and for all

--- src/helpers/publishGMA.ts
+++ src/helpers/publishGMA.ts
@@ -1,9 +1,8 @@
 import { ChildProcess } from "child_process";
-import { MAX_TIMEOUT } from "../constants";
 import fs from "fs";
 import { glob } from "glob";
 import path from "path";
-import { runCmd, spawnProcess } from "./runCmd";
+import { runCmd } from "./runCmd";
 
 function findFilePath(pattern: string) {
 	const matches = glob.sync(pattern, { nodir: true});
@@ -16,11 +15,24 @@ function findFilePath(pattern: string) {
 
 export default async function publishGMA(accountName: string, accountPassword: string, workshopId: string, changes: string, accountSecret: string) {
 	// can't rely on static paths because github action environment are idiotic
-	const gmaPath = findFilePath("**/addon.gma");
+	const gmaPath = path.resolve(findFilePath("**/addon.gma"));
 	const steamcmdPath = path.resolve(__dirname, "..", "..", "bin", "steamcmd.exe");
-	const gmPublishPath = path.resolve(__dirname, "..", "..", "bin", "gmpublish.exe");
 	const steamGuardPath = path.resolve(__dirname, "..", "..", "bin", "steam_guard.exe");
 	const passcodePath = path.resolve(__dirname, "..", "..", "bin", "passcode.txt");
+
+	const gma = fs.readFileSync(gmaPath).toString();
+	console.log(`Uploading GMA file: ${gmaPath} (${gma.length} bytes)`);
+
+	fs.writeFileSync("workshop.vdf", `"workshopitem"
+{
+	"appid" "4000"
+	"contentfolder" "${gmaPath}"
+	"changenote" "${changes.replace("\"", "")}"
+	"publishedfileid" "${workshopId}"
+}`);
+
+	const workshopVdfPath = path.resolve(findFilePath("**/workshop.vdf"));
+	console.log("Worshop VDF file created at: " + workshopVdfPath);
 
 	let err = null;
 	let twoFactorCode = null;
@@ -41,46 +53,25 @@ export default async function publishGMA(accountName: string, accountPassword: s
 	let steamCmdProc: ChildProcess;
 	try {
 		fs.chmodSync(steamcmdPath, "0755");
-		fs.chmodSync(gmPublishPath, "0755");
 
-		let steamCmd = `${steamcmdPath} +login ${accountName} ${accountPassword}`;
+		let steamCmd = `${steamcmdPath} +@ShutdownOnFailedCommand 1 +login ${accountName} ${accountPassword}`;
 		if (twoFactorCode) steamCmd += ` ${twoFactorCode}`;
 
-		let runSteamAgain = false;
-		let proceed = 0;
-		await runCmd(steamCmd, MAX_TIMEOUT / 4, (child: ChildProcess, data: string, _: string) => { // timeout / 4 because the max running time on github is 5 mins
+		steamCmd += ` +workshop_build_item "${workshopVdfPath}" +quit`;
+
+		await runCmd(steamCmd, (child: ChildProcess, data: string, _: string) => { // timeout / 4 because the max running time on github is 5 mins
 			if (data.startsWith("FAILED (Two-factor code mismatch")) {
 				child.kill(9);
-				runSteamAgain = true;
 			} else {
 				steamCmdProc = child;
 			}
-		}, (data: string) => {
-			if (data.endsWith("OK")) {
-				proceed++;
-			}
-
-			return proceed >= 4;
-		});
-
-		proceed = 0;
-		if (runSteamAgain) await runCmd(steamCmd, MAX_TIMEOUT, (child: ChildProcess, _: string, __: string) => { steamCmdProc = child; }, (data: string) => {
-			if (data.endsWith("OK")) {
-				proceed++;
-			}
-
-			return proceed >= 4;
-		});
-
-		await spawnProcess(gmPublishPath, ["update", "-addon", gmaPath, "-id", workshopId, "-changes", changes], {
-			detached: false,
-			shell: false,
 		});
 	} catch (e) {
 		err = e;
 	} finally {
 		fs.unlinkSync(gmaPath);
 		fs.unlinkSync(passcodePath);
+		fs.unlinkSync(workshopVdfPath);
 
 		if (steamCmdProc) steamCmdProc.kill(9);
 	}
